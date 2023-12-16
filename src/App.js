72726cabe0abe5563df38137464d3197310619a3
Musxeto <mustafamalikawan786@gmail.com>
2023-12-16 17:12:34 +0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header';
+import './App.css'; 
+import TransactionList from './Components/TransactionList';
+import Balance from './Components/Balance';
+import IncomeExpenses from './Components/IncomeExpenses';
+import AddTransaction from './Components/AddTransaction';
+import { GlobalProvider } from './Context/GlobalState';
 
-function App() {
+
+const MyApp = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header title="Kharcha Tracker" />
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </GlobalProvider>
   );
 }
 
-export default App;
+export default MyApp; 
