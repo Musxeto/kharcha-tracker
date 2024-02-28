315d7d75f1d3ba00c934d4f025da376b954b26fa
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-28 13:31:29 +0500

    css updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import Header from './Components/Header';
-import './App.css'; 
-import TransactionList from './Components/TransactionList';
-import Balance from './Components/Balance';
-import IncomeExpenses from './Components/IncomeExpenses';
-import AddTransaction from './Components/AddTransaction';
-import { GlobalProvider } from './Context/GlobalState';
-
+import React from "react";
+import Header from "./Components/Header";
+import "./App.css";
+import TransactionList from "./Components/TransactionList";
+import Balance from "./Components/Balance";
+import IncomeExpenses from "./Components/IncomeExpenses";
+import AddTransaction from "./Components/AddTransaction";
+import { GlobalProvider } from "./Context/GlobalState";
 
 const MyApp = () => {
   return (
-    <GlobalProvider>
-      <Header title="Kharcha Tracker" />
-      <div className='container'>
-        <Balance/>
-        <IncomeExpenses/>
-        <TransactionList/>
-        <AddTransaction/>
-      </div>
-    </GlobalProvider>
+    <div className="main">
+      <GlobalProvider>
+        <Header title="Kharcha Tracker" />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    </div>
   );
-}
+};
 
-export default MyApp; 
+export default MyApp;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Header = ({ title }) => {
-  return (
-    <h1>{title}</h1>
-  );
-}
+  return <h1 className="title">{title}</h1>;
+};
 
 export default Header;
